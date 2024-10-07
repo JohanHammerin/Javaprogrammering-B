9dc12f60e26d7270fe85b2ddc6bc7366423f095b
JohanHammerin <Johan04hammerin@gmail.com>
2024-10-07 11:49:57 +0200

    11:49

---
+++ src/main/java/se/johan_hammerin/Hawaii/Rectangle.java
@@ -0,0 +1,13 @@
+package se.johan_hammerin.Hawaii;
+
+public class Rectangle implements Shape{
+    @Override
+    public double getArea(double height, double width) {
+        return 0;
+    }
+
+    @Override
+    public double getCircumference() {
+        return 0;
+    }
+}
---
+++ src/main/java/se/johan_hammerin/Hawaii/Shape.java
@@ -0,0 +1,7 @@
+package se.johan_hammerin.Hawaii;
+
+public interface Shape {
+    //Methods
+    double getArea(double height, double width);
+    double getCircumference();
+}

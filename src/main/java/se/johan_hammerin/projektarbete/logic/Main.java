b427160ae1affd23791bdcb8c1e63f4697d2606c
JohanHammerin <Johan04hammerin@gmail.com>
2024-11-07 20:24:31 +0100

    20:24

--- src/main/java/se/johan_hammerin/projektarbete/logic/Main.java
+++ src/main/java/se/johan_hammerin/projektarbete/logic/Main.java
@@ -3,11 +3,25 @@ package se.johan_hammerin.projektarbete.logic;
 import se.johan_hammerin.projektarbete.model.Resident;
 import se.johan_hammerin.projektarbete.gui.GUI;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Resident hero = new Resident();
-        new Game(hero);
-        GUI gui = new GUI();
-        gui.createGUI(hero);
+        Scanner input = new Scanner(System.in);
+        if (greeting(input)) {
+            Resident hero = new Resident();
+            new Game(hero);
+            GUI gui = new GUI();
+            gui.createGUI(hero);
+        } else {
+            System.out.println("Synd för dig.\nSpelet stängs av...");
+            System.exit(0);
+        }
+    }
+
+    public static boolean greeting(Scanner input) {
+        System.out.println("Välkommen till äventyrsspelet!");
+        System.out.println("Är det inte lite tråkigt om hela spelet skulle bestå av text i konsolen?");
+        return input.next().equals("jo");
     }
 }

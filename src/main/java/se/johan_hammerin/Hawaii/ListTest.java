d6f6bcc8002a00ec0f013538afb07601c9b2c611
Johan Hammerin <Johan04hammerin@gmail.com>
2024-11-13 10:45:12 +0100

    10:45

---
+++ src/main/java/se/johan_hammerin/Hawaii/ListTest.java
@@ -0,0 +1,13 @@
+package se.johan_hammerin.Hawaii;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListTest {
+    public static void main(String[] args) {
+
+        List<String> myArrayList = new ArrayList<>();
+        myArrayList.add("Apple");
+        myArrayList.remove("Apple");
+    }
+}
